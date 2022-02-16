a599fff8b4f489eb2911005f9c0866cdce4b4fc0
PavanKumar-Gudiwada <91024463+PavanKumar-Gudiwada@users.noreply.github.com>
2022-02-16 19:37:20 +0530

    reduce CNN

--- earlyWarnings.c
+++ earlyWarnings.c
@@ -7,24 +7,43 @@
 
 static char * earlyStr[numberOfLanguages][2] = {{"Approaching minimum", "Approaching peak"},
 													{"Nähert sich dem Minimum","Nähert sich dem Maximum"}};
+													
+bool earlyWarningLow(float paramValue, float lowerLim, float lowerWarningLimit)
+{
+	if((paramValue > lowerLim)&&(paramValue < lowerWarningLimit)){
+		return 1;
+	}else{
+		return 0;
+	}
+}
+
+bool earlyWarningHigh(float paramValue, float upperLim, float upperWarningLimit)
+{
+	if((paramValue > upperWarningLimit)&&(paramValue < upperLim)){
+		return 1;
+	}else{
+		return 0;
+	}
+}													
 
 enum violate earlyWarning(float paramValue, float upperLim, float lowerLim)
 {
 	float lowerWarningLimit, upperWarningLimit, buffer;
-	
+	bool earlyWarningHighPresent, earlyWarningLowPresent;
 	buffer = EARLY_WARNING_PERCENT / 100.0 * upperLim;
 	
 	lowerWarningLimit = lowerLim + buffer;
 	upperWarningLimit = upperLim - buffer;
 	
-	if((paramValue > lowerLim)&&(paramValue < lowerWarningLimit)){
-		return 0;
-	}
-	else if((paramValue > upperWarningLimit)&&(paramValue < upperLim)){
-		return 1;
-	}
-	else{
-		return 2;
+	earlyWarningLowPresent = earlyWarningLow(paramValue, lowerLim, lowerWarningLimit);
+	earlyWarningHighPresent = earlyWarningHigh(paramValue, upperLim, upperWarningLimit);
+	
+	if(earlyWarningLowPresent){
+		return earlyLow;
+	}else if(earlyWarningHighPresent){
+		return earlyHigh;
+	}else{
+		return noEarlyWarning;
 	}
 	
 }
